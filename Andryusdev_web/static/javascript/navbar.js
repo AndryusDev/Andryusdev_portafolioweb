9a709dd1dfc135d1eb9350a7ecec71a3d0fd705b
AndryusDev <andryusdev@gmail.com>
2025-08-10 18:04:52 -0400

    arreglando animacion del menu

--- Andryusdev_web/static/javascript/navbar.js
+++ Andryusdev_web/static/javascript/navbar.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.querySelector('.navbar__toggle');
     const menu = document.querySelector('.navbar__menu-contenedor');
+    const opacidad = document.querySelector('.navbar__overlay');
     
     toggleButton.addEventListener('click', function() {
         menu.classList.toggle('active');
+        opacidad.classList.toggle('active');
+    });
+
+    document.querySelector('.navbar__overlay').addEventListener('click', function() {
+    if(this.classList.contains('active')) {
+        document.querySelector('.navbar__menu-contenedor').classList.remove('active');
+        this.classList.remove('active');
+        }
     });
 });
